5fe3b56217fe36990762fac10e734c97ae0c7c3d
Kate Mashokha <katyamashokha@yahoo.com>
2017-08-04 10:22:13 -0700

    Create test to make sure Event object instantiates correctly

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,4 +4,9 @@ package models;
  * Created by katsiarynamashokha on 8/4/17.
  */
 public class Event {
+
+    public Event(int numGuests, String foodType, String beverageType, String entertainmentType) {
+
+    }
+
 }
---
+++ src/test/java/models/EventTest.java
@@ -0,0 +1,21 @@
+package models;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by katsiarynamashokha on 8/4/17.
+ */
+public class EventTest {
+
+    @Test
+    public void newEvent_instantiatesCorrectly() throws Exception {
+        Event testEvent = new Event(10, "Light", "Juice bar", "Live band");
+        assertEquals(true, testEvent instanceof Event);
+    }
+
+
+}
