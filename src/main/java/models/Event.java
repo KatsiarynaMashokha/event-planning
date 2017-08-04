cd60d2671509da5c0984c39a372d0895ba7b24f0
Kate Mashokha <katyamashokha@yahoo.com>
2017-08-04 10:31:22 -0700

    Pass the test which makes sure that number of guest property is saved correctly

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -4,13 +4,15 @@ package models;
  * Created by katsiarynamashokha on 8/4/17.
  */
 public class Event {
+    private int numGuests;
 
     public Event(int numGuests, String foodType, String beverageType, String entertainmentType) {
+      this.numGuests = numGuests;
 
     }
 
     public int getNumGuests() {
-        return 0;
+        return numGuests;
     }
 
 }
